1d786963e881f3b50234a876fbfd176be14d2454
Mariví <marivirh82@gmail.com>
2018-07-11 17:49:40 +0200

    get series image and put in html

--- js/main.js
+++ js/main.js
@@ -2,18 +2,13 @@
 
 var seriesContainer = document.querySelector('.series__container');
 var seriesList = document.createElement('ul');
-var seriesItem = document.createElement('li');
 
 var url = 'http://api.tvmaze.com/search/shows?q=';
-var arraySeriesName;
-var arraySeriesImg;
 
 var userInput = document.querySelector('.input');
 var searchButton = document.querySelector('.button');
 
-
 seriesContainer.appendChild(seriesList);
-seriesList.appendChild(seriesItem);
 
 function takeUserSearch() {
   var inputValue = userInput.value;
@@ -24,10 +19,17 @@ function takeUserSearch() {
     .then(function(json){
       console.log('json', json);
       for (var i = 0; i < json.length; i++) {
-        arraySeriesName = json[i].show.name;
+        var arraySeriesName = json[i].show.name;
         console.log('series name', arraySeriesName);
-        //arraySeriesImg = json[[2].image.medium;
-        //console.log('imagen', arraySeriesImg);
+        var arraySeriesImg = json[i].show.image.medium;
+        console.log('imagen', arraySeriesImg);
+        var seriesItem = document.createElement('li');
+        var seriesImage = document.createElement('img');
+        seriesList.appendChild(seriesItem);
+        seriesItem.innerHTML = json[i].show.name;
+        seriesImage.setAttribute('src', json[i].show.image.medium);
+        seriesItem.appendChild(seriesImage);
+        console.log(seriesImage);
       }
     });
 }
